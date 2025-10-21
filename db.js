6672a7ab67bc08cc89fbf84eb94279e000ff1026
Leonardo Bobadilla <135734651+leoelpokemo@users.noreply.github.com>
2025-10-20 20:18:19 -0600

    Changes to be committed: 	new file:   .gitignore 	modified:   db.js

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 import  mongoose  from "mongoose";
-const MONGO_URI = "mongodb://localhost:27017/terceroa"
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/terceroa"
 
 export const connectDB = async () => {
   try {
