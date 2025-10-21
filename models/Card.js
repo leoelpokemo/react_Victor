ca80fdc901cb9476cc7a4a862a6e83ae1a249db1
Leonardo Bobadilla <135734651+leoelpokemo@users.noreply.github.com>
2025-10-20 20:35:37 -0600

    Changes to be committed: 	modified:   models/Card.js

--- models/Card.js
+++ models/Card.js
@@ -2,24 +2,10 @@ import mongoose from "mongoose";
 
 const cardSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    link: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
+    name: { type: String, required: true, trim: true },
+    link: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
-
 export const Card = mongoose.model("Card", cardSchema);
